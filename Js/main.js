3a8df47885c8de8ccba103f970dbf45477f35eae
Kerolos Sadek <kokosadek55@gmail.com>
2023-01-25 22:58:44 +0200

    Edit Form

--- Js/main.js
+++ Js/main.js
@@ -5,9 +5,3 @@ function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
 window.setTimeout(openForm, 4000);
-
-// document.getElementsByClassName("sproket").style.display = "none"
-
-setTimeout(() => {
-  document.getElementsByClassName("sproket").style.display = "none"
-}, 10000);
